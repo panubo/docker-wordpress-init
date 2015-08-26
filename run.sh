06cd9db393bd3e675abd7f11fd9834e81c536830
Andrew Cutler <macropin@gmail.com>
2015-08-26 14:30:20 +1000

    typo

--- run.sh
+++ run.sh
@@ -42,7 +42,7 @@ done
 printenv | sort > "${ENV_FILE}"
 
 # Update MySQL
-MYSQL="mysql --host=${DB_HOST}--user=root --password=$MYSQL_ENV_MYSQL_ROOT_PASSWORD"
+MYSQL="mysql --host=${DB_HOST} --user=root --password=$MYSQL_ENV_MYSQL_ROOT_PASSWORD"
 echo "CREATE DATABASE IF NOT EXISTS ${DB_NAME};" | $MYSQL
 echo "GRANT ALL ON ${DB_NAME}.* to ${DB_USER}@'%' IDENTIFIED BY '$DB_PASS';" | $MYSQL
 echo "FLUSH PRIVILEGES;" | $MYSQL
